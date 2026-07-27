254292c4d1282c91634a429ee70372e56c8370ba
radiationISGlobal <138576333+radiationISGlobal@users.noreply.github.com>
2026-07-27 13:46:41 +0200

    Update rERR_jf.R

--- rERR/rERR_jf.R
+++ rERR/rERR_jf.R
@@ -2,6 +2,8 @@
 library(stats4)
 library(dplyr)
 
+# If the ID variable contains any characters, update line 53 to use lapply(as.numeric) excluding the ID column.
+
 dyn.load("Y:/llhood_linear_v3.dll")
 
 #///////////////////////////////////////////////////////////////////////////////////////////////
